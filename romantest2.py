26d5bf0da5bc58d16b3b3edb6e806f7c1b13de85
hnr250 <hohner250@gmail.com>
2016-03-15 23:22:46 +0100

    kolejny test

--- romantest2.py
+++ romantest2.py
@@ -1,1 +1,10 @@
-__author__ = 'karol'
+import unittest
+import roman2
+
+class ToRomanBadInput(unittest.TestCase):
+    def test_too_large(self):
+        '''to_roman powinna zglosic wyjatek przy zbyt duzym parametrze'''
+        self.assertRaises(roman2.OutOfRangeError, roman2.to_roman, 4000)
+
+if __name__ == '__main__':
+    unittest.main()
